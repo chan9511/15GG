51554babedee713ff77706fca54c968e06a8cb6a
Chandong Kim <kcd9511@gmail.com>
2023-10-09 18:16:11 +0900

    스펠,아이템 생성

--- my-app/src/pages/anal/ClassicAn.js
+++ my-app/src/pages/anal/ClassicAn.js
@@ -8,8 +8,8 @@ const ClassicAn = () => {
   //     "http://ddragon.leagueoflegends.com/cdn/13.19.1/img/item/1001.png";
   //   const itembuild =
   //     "http://ddragon.leagueoflegends.com/cdn/13.19.1/img/item/3003.png";
-  //   const summonerspell =
-  //     "http://ddragon.leagueoflegends.com/cdn/13.19.1/img/spell/SummonerFlash.png";
+  const summonerspell =
+    "http://ddragon.leagueoflegends.com/cdn/13.19.1/img/spell/SummonerFlash.png";
   //   const cursor = "https://s-lol-web.op.gg/images/icon/icon-arrow-right.svg";
   //   const skillImageUrl =
   //     "https://opgg-static.akamaized.net/meta/images/lol/spell/AkaliQ.png?image=q_auto,f_webp,w_64&v=1696570752446";
@@ -42,15 +42,48 @@ const ClassicAn = () => {
           <div className="percent-table">50%</div>
         </div>
 
-        <div className="rune-table"></div>
-
         <div className="rune-table">
-          <div className="rune-sub">gd</div>
+          <div className="runetable-left">
+            룬페이지
+            <div>gdgdgd</div>
+            <div>gdgdgd</div>
+            <div>gdgdgd</div>
+            <div>gdgdgd</div>
+            <div>gdgdgd</div>
+            <div>gdgdgd</div>
+            <div>gdgdgd</div>
+            <div>gdgdgd</div>
+          </div>
+          
+          <div className="runetable-right">
+            소환사 주문
+            <div>
+              <img src={summonerspell} alt="spell" className="image-all" />
+              <img src={summonerspell} alt="spell" className="image-all" />
+              <div className="win-rate">승률:60%</div>
+            </div>
+          </div>
+          
+
+          <div className="runetable-right">
+            시작 아이템
+            <div>
+              <img src={summonerspell} alt="spell" className="image-all" />
+              <img src={summonerspell} alt="spell" className="image-all" />
+              <div className="win-rate">승률:60%</div>
+            </div>
+            
+          </div>
+          
         </div>
-        <div className="item-table">gd</div>
+
+        <div>ㅎㅇ</div>
+        
+
+        
       </main>
 
-      <aside></aside>
+      
     </div>
   );
 };
